805bed261d657ba196cf73bbb0ef56eb4f26ace4
Larissa Sena <larissasenar@gmail.com>
2025-05-05 13:41:59 -0300

    add

--- cotacao.py
+++ cotacao.py
@@ -1,7 +1,11 @@
 import requests
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
-API_KEY = "6JPCXIJ7G3QU388I"  #chave da Alpha Vantage
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")  # Carrega a chave do .env
 
 def buscar_cotacao(simbolo):
     url = f"https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol={simbolo}&apikey={API_KEY}"
